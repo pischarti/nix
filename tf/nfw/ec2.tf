c2bf7cb18b45316d0939116a892477e4174a0e76
steve <snguyen@valleyit.com>
2025-09-13 16:08:45 -0500

    add httpd

--- tf/nfw/ec2.tf
+++ tf/nfw/ec2.tf
@@ -30,10 +30,24 @@ resource "aws_instance" "app" {
   vpc_security_group_ids      = [aws_security_group.app.id]
   associate_public_ip_address = true
   key_name                    = coalesce(var.app_key_name, aws_key_pair.ssh_generated.key_name)
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -euxo pipefail
+              sudo yum update -y || true
+              sudo yum install -y httpd
+              sudo systemctl enable httpd
+              SVC=httpd
+              PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+              PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+              echo "<h1>App Server Details</h1><p><strong>Hostname:</strong> $(hostname)</p><p><strong>VPC:</strong> App VPC</p><p><strong>Private IP:</strong> $PRIVATE_IP</p><p><strong>Public IP:</strong> $PUBLIC_IP</p>" | sudo tee /var/www/html/index.html           
+              sudo systemctl restart "$SVC"
+              EOF
 
   tags = merge(
     {
-      Name = "${var.app_vpc_name}-app-ec2"
+      Name = "${local.name}-app-ec2"
     },
     var.tags
   )
@@ -51,10 +65,24 @@ resource "aws_instance" "egress" {
   vpc_security_group_ids      = [aws_security_group.egress.id]
   associate_public_ip_address = true
   key_name                    = coalesce(var.app_key_name, aws_key_pair.ssh_generated.key_name)
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -euxo pipefail
+              sudo yum update -y || true
+              sudo yum install -y httpd
+              sudo systemctl enable httpd
+              SVC=httpd
+              PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+              PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+              echo "<h1>App Server Details</h1><p><strong>Hostname:</strong> $(hostname)</p><p><strong>VPC:</strong> App VPC</p><p><strong>Private IP:</strong> $PRIVATE_IP</p><p><strong>Public IP:</strong> $PUBLIC_IP</p>" | sudo tee /var/www/html/index.html           
+              sudo systemctl restart "$SVC"
+              EOF
 
   tags = merge(
     {
-      Name = "${var.app_vpc_name}-egress-ec2"
+      Name = "${local.name}-egress-ec2"
     },
     var.tags
   )
