053469cabd28b026fc465b227b9e89ea0c84da9d
steve <snguyen@valleyit.com>
2025-09-24 14:21:30 -0500

    restructure spokes tf's

--- tf/nfw-tgw-spokes/inspection_vpc.tf
+++ tf/nfw-tgw-spokes/inspection_vpc.tf
@@ -30,29 +30,6 @@ resource "aws_subnet" "inspection_vpc_firewall_subnet" {
   })
 }
 
-resource "aws_internet_gateway" "inspection_vpc_igw" {
-  vpc_id = aws_vpc.inspection_vpc.id
-  tags = merge(local.tags, {
-    Name = "inspection-vpc/internet-gateway"
-  })
-}
-
-resource "aws_eip" "inspection_vpc_nat_gw_eip" {
-  count = length(local.selected_azs)
-}
-
-resource "aws_nat_gateway" "inspection_vpc_nat_gw" {
-  count         = length(local.selected_azs)
-  depends_on    = [aws_internet_gateway.inspection_vpc_igw, aws_subnet.inspection_vpc_public_subnet]
-  allocation_id = aws_eip.inspection_vpc_nat_gw_eip[count.index].id
-  subnet_id     = aws_subnet.inspection_vpc_public_subnet[count.index].id
-  tags = merge(local.tags, {
-    Name = "inspection-vpc/${local.selected_azs[count.index]}/nat-gateway"
-  })
-}
-
-
-
 resource "aws_subnet" "inspection_vpc_tgw_subnet" {
   count                   = length(local.selected_azs)
   map_public_ip_on_launch = false
@@ -77,12 +54,6 @@ resource "aws_route_table" "inspection_vpc_tgw_subnet_route_table" {
   })
 }
 
-resource "aws_route_table_association" "inspection_vpc_tgw_subnet_route_table_association" {
-  count          = length(local.selected_azs)
-  route_table_id = aws_route_table.inspection_vpc_tgw_subnet_route_table[count.index].id
-  subnet_id      = aws_subnet.inspection_vpc_tgw_subnet[count.index].id
-}
-
 resource "aws_route_table" "inspection_vpc_firewall_subnet_route_table" {
   count  = length(local.selected_azs)
   vpc_id = aws_vpc.inspection_vpc.id
@@ -97,12 +68,6 @@ resource "aws_route_table" "inspection_vpc_firewall_subnet_route_table" {
   tags = merge(local.tags, {
     Name = "inspection-vpc/${local.selected_azs[count.index]}/firewall-subnet-route-table"
   })
-}
-
-resource "aws_route_table_association" "inspection_vpc_firewall_subnet_route_table_association" {
-  count          = length(local.selected_azs)
-  route_table_id = aws_route_table.inspection_vpc_firewall_subnet_route_table[count.index].id
-  subnet_id      = aws_subnet.inspection_vpc_firewall_subnet[count.index].id
 }
 
 resource "aws_route_table" "inspection_vpc_public_subnet_route_table" {
@@ -121,8 +86,41 @@ resource "aws_route_table" "inspection_vpc_public_subnet_route_table" {
   })
 }
 
+resource "aws_route_table_association" "inspection_vpc_tgw_subnet_route_table_association" {
+  count          = length(local.selected_azs)
+  route_table_id = aws_route_table.inspection_vpc_tgw_subnet_route_table[count.index].id
+  subnet_id      = aws_subnet.inspection_vpc_tgw_subnet[count.index].id
+}
+
+resource "aws_route_table_association" "inspection_vpc_firewall_subnet_route_table_association" {
+  count          = length(local.selected_azs)
+  route_table_id = aws_route_table.inspection_vpc_firewall_subnet_route_table[count.index].id
+  subnet_id      = aws_subnet.inspection_vpc_firewall_subnet[count.index].id
+}
+
 resource "aws_route_table_association" "inspection_vpc_public_subnet_route_table_association" {
   count          = length(local.selected_azs)
   route_table_id = aws_route_table.inspection_vpc_public_subnet_route_table[count.index].id
   subnet_id      = aws_subnet.inspection_vpc_public_subnet[count.index].id
+}
+
+resource "aws_internet_gateway" "inspection_vpc_igw" {
+  vpc_id = aws_vpc.inspection_vpc.id
+  tags = merge(local.tags, {
+    Name = "inspection-vpc/internet-gateway"
+  })
+}
+
+resource "aws_eip" "inspection_vpc_nat_gw_eip" {
+  count = length(local.selected_azs)
+}
+
+resource "aws_nat_gateway" "inspection_vpc_nat_gw" {
+  count         = length(local.selected_azs)
+  depends_on    = [aws_internet_gateway.inspection_vpc_igw, aws_subnet.inspection_vpc_public_subnet]
+  allocation_id = aws_eip.inspection_vpc_nat_gw_eip[count.index].id
+  subnet_id     = aws_subnet.inspection_vpc_public_subnet[count.index].id
+  tags = merge(local.tags, {
+    Name = "inspection-vpc/${local.selected_azs[count.index]}/nat-gateway"
+  })
 }
